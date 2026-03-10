d031ecd580894f4e7479a1a14920ee85dce0922e
Jordanedwardlwyaw <jlwaya@byupathwy.edu>
2026-03-10 11:53:22 +0300

    Completed Chapter 5: Navigation and Active Links

--- nextjs-dashboard/app/ui/dashboard/nav-links.tsx
+++ nextjs-dashboard/app/ui/dashboard/nav-links.tsx
@@ -1,22 +1,27 @@
-'use client'; // 1. This must be at the top to use hooks!
+'use client';
 
 import {
   UserGroupIcon,
   HomeIcon,
   DocumentDuplicateIcon,
 } from '@heroicons/react/24/outline';
-import Link from 'next/link'; // 2. Import the Next.js Link component
-import { usePathname } from 'next/navigation'; // 3. Import the hook
-import clsx from 'clsx'; // 4. This helps with the blue highlight logic
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import clsx from 'clsx';
 
+// Map of links to display in the side navigation.
 const links = [
   { name: 'Home', href: '/dashboard', icon: HomeIcon },
-  { name: 'Invoices', href: '/dashboard/invoices', icon: DocumentDuplicateIcon },
+  {
+    name: 'Invoices',
+    href: '/dashboard/invoices',
+    icon: DocumentDuplicateIcon,
+  },
   { name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
 ];
 
 export default function NavLinks() {
-  const pathname = usePathname(); // 5. Get the current URL path
+  const pathname = usePathname();
 
   return (
     <>
@@ -26,7 +31,6 @@ export default function NavLinks() {
           <Link
             key={link.name}
             href={link.href}
-            // 6. This logic makes the tab turn BLUE when active
             className={clsx(
               'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
               {
